1af294e6fa184a0b181fcbfb556430ae88a7681a
crazykeks <voloshinwoolf24@gmail.com>
2018-08-09 17:07:13 +0300

    работа со state React (удаление элемента списка)

--- src/App.js
+++ src/App.js
@@ -20,27 +20,52 @@ const list = [
   },
 ];
 
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+    console.log(updatedList);
+  }
+
+
+
+
   render() {
     return (
       <div className="App">
-        {list.map(function (item) {
-          return (
+        {this.state.list.map(item =>
             <div key={item.objectID}>
               <span>
-               <a href={item.url}>{item.title}</a>
+              <a href={item.url}>{item.title}</a>
               </span>
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={()=>this.onDismiss(item.objectID)}
+                  type="button"
+                >Отбросить</button>
+              </span>
             </div>
-          );
-        })}
+        )}
       </div>
+
     );
   }
 }
+
+const a = new App();
+console.log(a);
 
 
 
